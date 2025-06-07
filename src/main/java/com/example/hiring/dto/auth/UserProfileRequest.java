1fd4239ee89afeb76ef9c6d4ce5797593d6c82c4
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-06-07 17:38:08 +0530

    request refactor

--- src/main/java/com/example/hiring/dto/auth/UserProfileRequest.java
+++ src/main/java/com/example/hiring/dto/auth/UserProfileRequest.java
@@ -7,9 +7,9 @@ import java.util.List;
 @Data
 public class UserProfileRequest {
     private String userId;
-    private PersonalInfoDto personalInfoDto;
-    private EmploymentDetailsDto employmentDetailsDto;
-    private List<ProjectDto> projectDtos;
-    private QualificationsDto qualificationsDto;
+    private PersonalInfoDto personalInfo;
+    private EmploymentDetailsDto employmentDetails;
+    private List<ProjectDto> projects;
+    private QualificationsDto qualifications;
     private ProfileOverviewDto profileOverview;
 }
