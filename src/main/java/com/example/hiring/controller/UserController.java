32ac1790be855384224a36654bb5e35d1d601d97
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-06-07 09:46:58 +0530

    new code

--- src/main/java/com/example/hiring/controller/UserController.java
+++ src/main/java/com/example/hiring/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/user")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
 
