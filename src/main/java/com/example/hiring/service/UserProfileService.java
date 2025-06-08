f65a2046b12f41b9de4b7bcf4a2d1bfa47c50cb6
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-06-08 05:35:15 +0530

    new refactored code

--- src/main/java/com/example/hiring/service/UserProfileService.java
+++ src/main/java/com/example/hiring/service/UserProfileService.java
@@ -1,13 +1,18 @@
 package com.example.hiring.service;
 
 import com.example.hiring.dto.auth.UserProfileDto;
+import com.example.hiring.entity.User;
 import com.example.hiring.entity.UserProfile;
 import com.example.hiring.repository.UserProfileRepository;
+import com.example.hiring.repository.UserRepository;
 import com.example.hiring.util.UserProfileConverter;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,10 +22,21 @@ public class UserProfileService {
     @Autowired
     private UserProfileRepository userProfileRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
     public UserProfile saveUserProfile(UserProfileDto request) {
         UserProfile profile = UserProfileConverter.convertRequestToEntity(request);
         log.info("user req:{}",profile);
-        return userProfileRepository.save(profile);
+        UserProfile save = userProfileRepository.save(profile);
+        Optional<User> userById = userRepository.findById(request.getUserId());
+            if (userById.isPresent()) {
+                User user = userById.get();
+                user.setSetupComplete(true);
+                userRepository.save(user);
+            }
+            return save;
     }
 
     public UserProfileDto getProfileById(Long id) {
